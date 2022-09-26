e40ba81d5c478b954e4dff9188dfdaa18da0ab57
lennoxstark47 <twisamhaldar@gmail.com>
2022-09-27 02:58:31 +0530

    Showing Changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, {
 	useState,
 	useEffect,
 } from 'react';
+import './dashboard.css';
 import axios from 'axios';
 
 export default function Dashboard() {
@@ -18,7 +19,7 @@ export default function Dashboard() {
 		axios
 			.get('http://localhost:5000/openingPrice')
 			.then((data) => {
-				console.log(data.data.data);
+				// console.log(data.data.data);
 				setStockData(data.data.data);
 			})
 			.catch((err) => {
@@ -26,30 +27,77 @@ export default function Dashboard() {
 			});
 	}, [temp]);
 
+	// useEffect(() => {
+	// 	for (let i = 0; i < stockData.length; i++) {
+	// 		console.log(
+	// 			stockData[i].stockData[
+	// 				stockData.length - 1
+	// 			]
+	// 		);
+	// 	}
+	// }, [stockData]);
+
+	useEffect(() => {
+		console.log(stockData);
+	}, [stockData]);
+
 	return (
-		<div>
-			<div
-				style={{
-					display: 'flex',
-					flexWrap: 'wrap',
-				}}>
-				{stockData.map((stck) => {
+		<div className='dashboard'>
+			<table>
+				<tr>
+					<th>Name</th>
+					<th>Opening Price</th>
+					<th>Current Price</th>
+					<th>Changes</th>
+				</tr>
+				{stockData.map((val, key) => {
+					let change =
+						((val.price - val.openingPrice) *
+							100) /
+						val.openingPrice;
 					return (
-						<div
-							style={{
-								width: '150px',
-								height: '150px',
-								borderStyle: 'groove',
-							}}>
-							<p>{stck.ticker}</p>
-							<p>
-								Opening Price: {stck.openingPrice}
-							</p>
-							<p>Current Price: {stck.price}</p>
-						</div>
+						<tr key={key}>
+							<td>{val.ticker}</td>
+							<td>{val.openingPrice}</td>
+							<td>{val.price}</td>
+							{change > 0 ? (
+								<td style={{ color: 'green' }}>
+									{change}%
+								</td>
+							) : (
+								<td style={{ color: 'red' }}>
+									{change}%
+								</td>
+							)}
+						</tr>
 					);
 				})}
-			</div>
+			</table>
 		</div>
+
+		// <div>
+		// 	<div
+		// 		style={{
+		// 			display: 'flex',
+		// 			flexWrap: 'wrap',
+		// 		}}>
+		// 		{stockData.map((stck) => {
+		// 			return (
+		// 				<div
+		// 					style={{
+		// 						width: '150px',
+		// 						height: '150px',
+		// 						borderStyle: 'groove',
+		// 					}}>
+		// 					<p>{stck.ticker}</p>
+		// 					<p>
+		// 						Opening Price: {stck.openingPrice}
+		// 					</p>
+		// 					<p>Current Price: {stck.price}</p>
+		// 				</div>
+		// 			);
+		// 		})}
+		// 	</div>
+		// </div>
 	);
 }
